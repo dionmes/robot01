3d2cd15a89e49362f868e20ba06b72eee61e8503
dion <d.mes@linframe.nl>
2024-06-02 11:35:34 +0200

    Added register API calls

--- Sappie_Master/sappieserver.py
+++ Sappie_Master/sappieserver.py
@@ -3,7 +3,7 @@ from flask import Flask, send_from_directory, render_template, jsonify, request
 sappie_state = {}
 
 #
-context = {"sappie_ip":"192.168.133.75", "sappiesense_ip" : "192.168.133.248", "sappie" : sappie_state}
+context = {"sappie_ip":"", "sappiesense_ip" : "", "sappie" : sappie_state}
 
 # Set static_folder to serve static files from 'static' directory
 app = Flask(__name__, static_folder='static')
@@ -32,10 +32,18 @@ def serve_images(filename):
 
 
 # API Endpoints
-@app.route('/api/greet', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'World')
-    return jsonify({'message': f'Hello, {name}!'})
+@app.route('/api/register_sappie', methods=['GET'])
+def register():
+    sappie_ip = request.args.get('ip')
+    context['sappie_ip'] = sappie_ip
+    return "ok"
+
+@app.route('/api/register_sense', methods=['GET'])
+def register():
+    sense_ip = request.args.get('ip')
+    context['sappiesense_ip'] = sense_ip
+    return "ok"
+
 
 # Example API endpoint that echoes posted JSON data
 @app.route('/api/echo', methods=['POST'])
