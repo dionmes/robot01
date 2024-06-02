1d9a7752eabaee768b8318633a7e8b04e58cd0d2
dion <d.mes@linframe.nl>
2024-06-02 21:00:29 +0200

    Add TTS capability and fixes

--- Sappie_Master/sappieserver.py
+++ Sappie_Master/sappieserver.py
@@ -1,5 +1,10 @@
 from flask import Flask, send_from_directory, render_template, jsonify, request
+import ipaddress
+import requests
 
+from speecht5udp import TTS
+
+tts = TTS()
 sappie_state = {}
 
 #
@@ -13,7 +18,7 @@ app = Flask(__name__, static_folder='static')
 def index(context=context):
     return render_template('index.html',context=context)
     
-@app.route('/<path:filename>')
+@app.route('/<filename>')
 def serve_html(filename,context=context):
     return render_template(filename,context=context) 
 # Serve CSS files
@@ -34,20 +39,61 @@ def serve_images(filename):
 @app.route('/api/register_sappie', methods=['GET'])
 def register_sappie():
     sappie_ip = request.args.get('ip')
-    context['sappie_ip'] = sappie_ip
-    return "ok"
+    
+    if validate_ip_address(sappie_ip):
+        context['sappie_ip'] = sappie_ip
+        api_response = { "status": "IP registration @ master successful", "error" : 0 }    
+    else:
+        context['sappie_ip'] = ""
+        api_response = { "status": "IP registration @ master failed", "error" : 101 }        
+    return jsonify(api_response)
 
 @app.route('/api/register_sense', methods=['GET'])
 def register_sense():
     sense_ip = request.args.get('ip')
-    context['sappiesense_ip'] = sense_ip
-    return "ok"
+
+    if validate_ip_address(sappie_ip):
+        context['sappiesense_ip'] = sappie_ip
+        api_response = { "status": "IP registration @ master successful", "error" : 0 }    
+    else:
+        context['sappiesense_ip'] = ""
+        api_response = { "status": "IP registration @ master failed", "error" : 101 }        
+    return jsonify(api_response)
+
+
+@app.route('/api/tts', methods=['POST'])
+def tts_api():
+    data = request.get_json()
+    text = data["text"]
+    
+    if context['sappie_ip'] != "":
+        # Call to enable Audio receive
+        requests.get("http://" + context['sappie_ip'] + "/audiostream?on=1")
+        # TTS
+        tts.speak(text,context['sappie_ip'])
+        # Call to stop Audio receive
+        requests.get("http://" + context['sappie_ip'] + "/audiostream?on=0")
+        
+        api_response = { "status": "ok", "error" : 0 }
+    else:
+        api_response = { "status": "Error", "error" : 102}
+  
+    return jsonify(api_response)
+
+# Validate IP Addresses
+def validate_ip_address(ip_string):
+    try:
+        ip_object = ipaddress.ip_address(ip_string)
+        return True
+    except ValueError:
+        return False
 
 # Example API endpoint that echoes posted JSON data
 @app.route('/api/echo', methods=['POST'])
 def echo():
     data = request.get_json()
     return jsonify(data)
+
 
 #
 #
